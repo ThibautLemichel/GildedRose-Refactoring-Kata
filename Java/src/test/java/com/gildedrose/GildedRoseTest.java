6b7e022a173a6d94005be8a184c9527245452f29
Antonije Stefanovic <a.stefanovic@datentraeger.at>
2025-03-26 19:05:31 +0100

    First test cases.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,38 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GildedRoseTest {
+import java.util.stream.Stream;
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+public class GildedRoseTest {
+
+    private GildedRose app;
+    private Item[] items;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[]{};
+        app = new GildedRose(items);
     }
 
+    @ParameterizedTest
+    @MethodSource("itemProvider")
+    void updateQualityDifferentItemsReturnsExpectedQuality(String name, int sellIn, int quality, int expectedQuality) {
+        items = new Item[]{ new Item(name, sellIn, quality) };
+        app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(expectedQuality, items[0].quality);
+    }
+
+    static Stream<Object[]> itemProvider() {
+        return Stream.of(
+            new Object[]{"Sulfuras, Hand of Ragnaros", 10, 35, 35},
+            new Object[]{"Sulfuras, Hand of Ragnaros", 5, 80, 80}
+        );
+    }
 }
